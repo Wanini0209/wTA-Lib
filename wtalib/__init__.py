590fbe6b62efd49142e68cc3f632912f754c3e94
Ting-Hsu Chang <wanini.klp.mashimaro@gmail.com>
2021-11-08 14:52:04 +0800

    initial commit

---
+++ wtalib/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+#          _|__   __  | . |__
+# \  /\  /  |    /  | | | |  |
+#  \/  \/   |__ /__/| | | |__|
+#
+
+"""
+Wanini's Financial Technical Analysis Library
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+wTA-Lib is a financial technical analysis library, written in Python.
+
+:copyright: (c) 2021 by Ting-Hsu Chang.
+:license: MIT, see LICENSE for more details.
+"""
