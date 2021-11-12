8faa7ede22bbb84d9b37042b893d51a346a87862
Ting-Hsu Chang <wanini.klp.mashimaro@gmail.com>
2021-11-12 13:06:51 +0800

    feat: Add `TimeSeries`
    Time-Series is a sequence of data points indexed by time which
    supports operations as follows:
    - attributes(writable):
      `name`.
    - properties(read-only):
      `data`, `index`, and `dtype`.
    - methods:
      `len`, `rename`, `to_pandas`, `fillna`, `ffill`, `bfill`, and
      `dropna`.

--- wtalib/_td/__init__.py
+++ wtalib/_td/__init__.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 """Time-Indexing Data.
 
+This package provides classes and utilities relative to time-indexing data.
+
+Classes
+-------
+TimeSeries : A sequence of data points indexed by time.
+
 """
+
+# pylint: disable=unused-import
+from ._series import TimeSeries  # noqa: F401
+
+# pylint: enable=unused-import
+
+__ALL__ = ['TimeSeries']
